ceba64293e2752f8980a958681c64745de644799
nhuanthi <tlam1288@gmail.com>
2020-06-16 18:11:48 -0700

    created querySelector variables for divs on the html. created a variable to hold an array with possible questions

---
+++ Assets/script.js
@@ -0,0 +1,16 @@
+var quiz = document.querySelector("#quiz");
+var question = document.querySelector("#question");
+var answerA = document.querySelector("#A");
+var answerB = document.querySelector("#B");
+var answerC = document.querySelector("#C");
+
+//all questions are stored in this array
+var multipleChoice = [
+  {
+    question: "Which of these is a Boolean?",
+    answerA: "False",
+    answerB: "56",
+    answerC: "Undefined",
+    correct: "A",
+  },
+];
